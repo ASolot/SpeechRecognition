6daf7edaa02829229c66e0c0d52d85c6c2875436
Alexandru Solot <solot.alexandru@yahoo.co.uk>
2017-01-21 23:12:45 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoogleCloudSamples;
+
+namespace IdentifyWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path;
+            string newFileName = "";
+           
+            Console.WriteLine("Enter folder path: ");
+            path = Console.ReadLine();
+
+            foreach (string file in Directory.GetFiles(path, "*.wav").Select(Path.GetFileName))
+            {
+                Console.Write("Current converting file: ");
+                Console.WriteLine(file);
+
+                Transcribe.translate(path + "\\" + file);
+                
+
+                // TODO: rename files with the word discovered
+                /*try
+                {
+                    if (File.Exists(path + "\\" + newFileName))
+                    {
+                        System.IO.File.Delete(path + "\\" + newFileName);
+                    }
+                    System.IO.File.Move(path + "\\" + file, path + "\\" + newFileName);
+
+                    Console.WriteLine("File renamed to: " + newFileName);
+                }
+                catch(Exception e)
+                {
+
+                }
+                finally
+                {
+                    
+                }*/
+
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Translate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using Google.Apis.CloudSpeechAPI.v1beta1;
+using Google.Cloud.Speech.V1Beta1;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using System.IO;
+
+namespace GoogleCloudSamples
+{
+    public class Transcribe
+    {
+        
+        static public CloudSpeechAPIService CreateAuthorizedClient()
+        {
+            GoogleCredential credential =
+                GoogleCredential.GetApplicationDefaultAsync().Result;
+            if (credential.IsCreateScopedRequired)
+            {
+                credential = credential.CreateScoped(new[]
+                {
+                    CloudSpeechAPIService.Scope.CloudPlatform
+                });
+            }
+            return new CloudSpeechAPIService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "wordidentify",
+            });
+        }
+
+        static public void translate(string args)
+        {
+            if (args == "")
+            {
+                Console.WriteLine("Usage:\nTranscribe audio_file");
+                return;
+            }
+            var service = CreateAuthorizedClient();
+            string audio_file_path = args;
+            var request = new Google.Apis.CloudSpeechAPI.v1beta1.Data.SyncRecognizeRequest()
+            {
+                Config = new Google.Apis.CloudSpeechAPI.v1beta1.Data.RecognitionConfig()
+                {
+                    Encoding = "LINEAR16",
+                    SampleRate = 44100,
+                    LanguageCode = "ro-RO"
+                },
+                Audio = new Google.Apis.CloudSpeechAPI.v1beta1.Data.RecognitionAudio()
+                {
+                    Content = Convert.ToBase64String(File.ReadAllBytes(audio_file_path))
+                }
+            };
+            var response = service.Speech.Syncrecognize(request).Execute();
+            foreach (var result in response.Results)
+            {
+                foreach (var alternative in result.Alternatives)
+                    Console.WriteLine(alternative.Transcript);
+            }
+        }
+    }
+}
